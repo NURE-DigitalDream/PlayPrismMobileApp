1dab560b182763fee1ebdbb65c6d6909e9b34438
NightStrang6r <tsaruk-2015@mail.ru>
2023-05-01 19:09:29 +0300

    🦀 Added more components to login page

---
+++ app/app/src/main/java/com/example/playprism/ui/login/LoginFragment.java
@@ -0,0 +1,32 @@
+package com.example.playprism.ui.login;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.playprism.databinding.FragmentLoginBinding;
+
+public class LoginFragment extends Fragment {
+
+    private FragmentLoginBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        getContext().getTheme().applyStyle(getId(), true);
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
